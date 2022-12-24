43696b29353e2e4c17740dfe255e0dbf116bb641
Celal Yasin Nari <celalyasinnari@gmail.com>
2022-12-24 18:17:46 +0200

    v 0.5

--- my-app/src/components/Footer.tsx
+++ my-app/src/components/Footer.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
+
 const Footer = () => {
     return (
         <div className='rounded-full py-3 px-6 bg-green-200 text-center'>
-            <button className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded'>Support Developer</button>
-
+            <a href="https://www.buymeacoffee.com/cynfinitely" className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded'>Support Developer</a>
         </div>
     )
 }
--- my-app/src/components/Main.tsx
+++ my-app/src/components/Main.tsx
@@ -2,19 +2,28 @@ import React from 'react'
 import "../assets/css/Main.css"
 import Ronaldo from "../assets/images/Ronaldo.jpg"
 import Messi from "../assets/images/Messi.jpg"
+import type { RootState } from '../redux/store'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../redux/slices/counterSlice'
 
 const Main = () => {
+    const count = useSelector((state: RootState) => state.counter.value)
+    const dispatch = useDispatch()
+
     return (
         <div>
             <div className='flex flex-row justify-evenly'>
                 <div className='py-2 m-2'>
                     <img src={Ronaldo} alt="Ronaldo" />
-                    <button className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded'>Support Ronaldo</button>
+                    <button className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded'
+                        onClick={() => dispatch(increment())}>Support Ronaldo</button>
+                    <p> Ronaldo Supported {count} time</p>
 
                 </div>
                 <div className='py-2 m-2'>
                     <img src={Messi} alt="Messi" />
-                    <button className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded'>Support Messi</button>
+                    <button className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded' onClick={() => dispatch(increment())}>Support Messi</button>
+                    <p> Messi Supported {count} time</p>
 
                 </div>
 
