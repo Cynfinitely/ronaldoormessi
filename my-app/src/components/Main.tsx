9fb5fa1c7ed876e36897ff2954fea21de0d82d35
Celal Yasin Nari <celalyasinnari@gmail.com>
2023-01-04 15:10:42 +0200

    main v1

--- my-app/src/components/Main.tsx
+++ my-app/src/components/Main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import "../assets/css/Main.css"
 import Ronaldo from "../assets/images/Ronaldo.jpg"
 import Messi from "../assets/images/Messi.jpg"
@@ -13,18 +12,20 @@ const Main = () => {
     return (
         <div className='h-full'>
             <div className='compare flex flex-row justify-evenly h-full'>
-                <div className='py-2 m-2'>
-                    <img src={Ronaldo} alt="Ronaldo" />
-                    <button className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded'
+                <div className='py-2 m-2 flex items-center'>
+                    <div className="flex flex-col items-center">
+                        <img src={Ronaldo} alt="Ronaldo" />
+                        <button className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded'
                         onClick={() => dispatch(increment())}>Support Ronaldo</button>
-                    <p> Ronaldo Supported {count} time</p>
-
+                        <p> Ronaldo Supported {count} time</p>
+                    </div>
                 </div>
-                <div className='py-2 m-2'>
-                    <img src={Messi} alt="Messi" />
-                    <button className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded' onClick={() => dispatch(increment())}>Support Messi</button>
-                    <p> Messi Supported {count} time</p>
-
+                <div className='py-2 m-2 flex items-center'>
+                    <div className="flex flex-col items-center">
+                        <img src={Messi} alt="Messi" />
+                        <button className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded' onClick={() => dispatch(increment())}>Support Messi</button>
+                        <p> Messi Supported {count} time</p>
+                    </div>                    
                 </div>
 
 
