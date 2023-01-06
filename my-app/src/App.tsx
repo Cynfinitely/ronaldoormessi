56c88718b3decf5fc3c9ab4003dac44ccb396319
Celal Yasin Nari <celalyasinnari@gmail.com>
2023-01-06 18:40:21 +0200

    redux_firebase

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,18 +3,22 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import db from "./firebase/firebase";
-import getCities from "./handlers/fireHandler";
 import incrementMessi from "./handlers/messiIncrementHandler";
 import incrementRonaldo from "./handlers/ronaldoIncrementHandler";
 import { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "./redux/store";
 
 function App() {
-  const dataRef: any = useRef();
+  const ronaldoCount = useSelector(
+    (state: RootState) => state.ronaldoCounter.value
+  );
+
+  const dispatch = useDispatch();
 
   const submithandler = (e: any) => {
     e.preventDefault();
     console.log(db);
-    getCities(db);
   };
   const incrementhandler = (e: any) => {
     e.preventDefault();
@@ -24,11 +28,7 @@ function App() {
     <div className="container flex flex-col">
       <Header />
       <Main />
-      <button
-        type="submit"
-        onClick={submithandler}>
-        TEST
-      </button>
+      <button type="submit">TEST</button>
       <button
         className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
         type="submit"
--- my-app/src/redux/slices/ronaldoCounterSlice.tsx
+++ my-app/src/redux/slices/ronaldoCounterSlice.tsx
@@ -1,5 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
-import type { PayloadAction } from "@reduxjs/toolkit";
+import type { PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
+import { doc, updateDoc, increment } from "firebase/firestore";
+import db from "../../firebase/firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 export interface CounterState {
   value: number;
@@ -13,20 +16,20 @@ export const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
-    ronaldoIncrement: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1;
-    },
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload;
+    ronaldoIncrement: () => {
+      const ronaldoRef = doc(db, "ronaldoLikes", "1");
+
+      updateDoc(ronaldoRef, {
+        likes: increment(1),
+      });
+
+      console.log("You are Succ!");
     },
   },
+  extraReducers: (builder) => {},
 });
 
 // Action creators are generated for each case reducer function
-export const { ronaldoIncrement, incrementByAmount } = counterSlice.actions;
+export const { ronaldoIncrement } = counterSlice.actions;
 
 export default counterSlice.reducer;
